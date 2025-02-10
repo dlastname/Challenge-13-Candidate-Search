beee8f3d9904c2950f4cfbb4ebe005f8d6da72b5
dlastname <dllorens28@gmail.com>
2025-02-10 17:26:02 -0500

    added NavBar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,33 @@
+// TODO: Add necessary code to display the navigation bar and link between the pages
+
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
   return (
-    <div>Nav</div>
-  )
+    <div className="nav">
+      <Link
+        key={1}
+        className={
+          currentPage === "/" ? "active nav-link nav-item" : "nav-link nav-item"
+        }
+        to="/"
+      >
+        Home
+      </Link>
+      ,
+      <Link
+        key={2}
+        className={
+          currentPage === "/SavedCandidates" ? "active nav-link nav-item" : "nav-link nav-item"
+        }
+        to="/SavedCandidates"
+      >
+        Potential Candidates
+      </Link>
+      ,
+    </div>
+  );
 };
 
 export default Nav;
